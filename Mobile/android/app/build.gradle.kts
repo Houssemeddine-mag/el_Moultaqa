99e581de8a3a00fe248c16ff9edd40bc9d0646c9
Mohcene dhia eddine <aimoh25ai@gmail.com>
2026-07-10 19:06:46 +0100

    fix: manual NDK install to avoid CI zip corruption + license issue

--- Mobile/android/app/build.gradle.kts
+++ Mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.elmoultaqa_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.0.12674087"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
